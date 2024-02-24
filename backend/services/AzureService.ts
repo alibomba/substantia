58a8e52e3793b69b79f1e894163a4fe19d1e5965
alibomba <wojci.bro@gmail.com>
2024-02-24 19:21:43 +0100

    azure post object

--- backend/services/AzureService.ts
+++ backend/services/AzureService.ts
@@ -21,6 +21,20 @@ class AzureService {
         const blobClient = containerClient.getBlobClient(path);
         return `${blobClient.url}?${containerSAS}`;
     }
+
+    public async postAzureObject(fileBuffer: Buffer, path: string, contentType: string) {
+        const accountName = process.env.AZURE_ACCOUNT_NAME as string;
+        const accountKey = process.env.AZURE_ACCOUNT_KEY as string;
+        const containerName = process.env.AZURE_CONTAINER_NAME as string;
+        const blobServiceClient = new BlobServiceClient(
+            `https://${accountName}.blob.core.windows.net`,
+            new StorageSharedKeyCredential(accountName, accountKey)
+        );
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+
+        const blockBlobClient = containerClient.getBlockBlobClient(path);
+        return await blockBlobClient.upload(fileBuffer, fileBuffer.length, { blobHTTPHeaders: { blobContentType: contentType } });
+    }
 }
 
 export default new AzureService();
